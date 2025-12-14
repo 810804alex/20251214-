c78b5977ad960079ec9a19f988e024ea0ed7bbf9
810804alex <326pp326qq@gmail.com>
2025-12-14 20:55:36 +0800

    Delete babel.config.js

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-// babel.config.js
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    // ⬇️ 把下面這一行刪掉或是註解起來 //
-    // plugins: ['react-native-reanimated/plugin'], 
-  };
-};
